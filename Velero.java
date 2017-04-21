06af48635e983431b4eac0040bf026540102d40d
naxoozil <nachoviuda@gmail.com>
2017-04-21 13:17:37 +0200

    commit de correccion

---
+++ Velero.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Velero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Velero
+{
+    // instance variables - replace the example below with your own
+    private int numMastiles;
+
+    /**
+     * Constructor for objects of class Velero
+     */
+    public Velero()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String toString()
+    {
+        // put your code here
+        return "0";
+    }
+    
+    public int getCoeficienteDeBernua()
+    {
+        return 0;
+    }
+}
